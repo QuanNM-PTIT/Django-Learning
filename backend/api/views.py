d0cb006326bb763449e991df3897cb433fda8cca
cityuongbi <quannm.ptit@gmail.com>
2023-08-04 11:14:37 +0700

    Use api_view to create apis

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,13 @@
-import json
-from django.http import JsonResponse
+from rest_framework.response import Response
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
 from products.models import Product
 
-
+@api_view(['GET', 'POST'])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data) # model_to_dict(model_data, fields=["title", "price"])
-        
-    return JsonResponse(data)
+        # model_to_dict(model_data, fields=["title", "price"])
+        data = model_to_dict(model_data)
+    return Response(data)
