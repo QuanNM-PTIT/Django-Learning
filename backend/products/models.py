bb9b61c92d75be444536ac9dafbd89c7fbcae6b3
cityuongbi <quannm.ptit@gmail.com>
2023-08-04 16:17:02 +0700

    Use serializers to validate input data

--- backend/products/models.py
+++ backend/products/models.py
@@ -4,3 +4,7 @@ class Product(models.Model):
     title       = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=100)
+    
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.9)
